7a6b230de9ac73ffc85838d5115cec931407cf03
nicholas.galazzo <nicholas.galazzo@gmail.com>
2021-01-30 10:10:17 -0500

    initial checkin

---
+++ ICarltonComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Carlton.Base.Client.State
+{
+    public interface ICarltonComponent<TViewModel>
+    {
+        TViewModel ViewModel { get; set; }
+        EventCallback OnComponentEvent { get; init; }
+    }
+}
